964eea5abc1777e32d00936956dcf128ecf48212
Nathãn Vitor de Lima <nathan2001lima@gmail.com>
2022-10-28 09:52:07 -0300

    pequenas refatorações e adiciona favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
+import refreshSvg from './assets/refresh.svg';
 
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-const diceRoll = (sides) => randomInt(1, sides);
+function diceRoll(sides) {
+  return randomInt(1, sides);
+}
 
 function fudgeRoll() {
   const possibleResults = ['-', ' ', '+'];
@@ -72,36 +75,19 @@ function App() {
   ));
 
   return (
-    <div>
-      <div style={{ display: 'flex' }}>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
+    <>
+      <div id="mainButtons">
+        <div id="operators">
           <button type="button" onClick={clearDices}>c</button>
           <button type="button" onClick={rerollDices}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-              <path fill="none" d="M0 0h24v24H0z" />
-              <path d="M5.463 4.433A9.961 9.961 0 0 1 12 2c5.523 0 10 4.477 10 10 0 2.136-.67 4.116-1.81 5.74L17 12h3A8 8 0 0 0 6.46 6.228l-.997-1.795zm13.074 15.134A9.961 9.961 0 0 1 12 22C6.477 22 2 17.523 2 12c0-2.136.67-4.116 1.81-5.74L7 12H4a8 8 0 0 0 13.54 5.772l.997 1.795z" />
-            </svg>
+            <img src={refreshSvg} alt="refresh" width="20px" />
           </button>
         </div>
-        <div style={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(auto-fill, 4em)',
-          width: '100%',
-        }}
-        >
-          {diceButtons}
-        </div>
+        <div id="diceButtons">{diceButtons}</div>
       </div>
 
-      <div style={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        minHeight: '5em',
-      }}
-      >
-        {visualDice}
-      </div>
-    </div>
+      <div id="dices">{visualDice}</div>
+    </>
   );
 }
 
